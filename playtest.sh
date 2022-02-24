504c5e6ba656b51d13710134fd0c680f671021f6
StarUndrscre <53362212+StarUndrscre@users.noreply.github.com>
2022-02-23 16:13:38 -0800

    Switch image sound, moved directories, set-up game script

--- playtest.sh
+++ playtest.sh
@@ -4,5 +4,9 @@ if [ ! -d "Regrown" ]; then
     git clone https://github.com/Sp1d3rmanj0e/Regrown.git
 fi
 cd Regrown
+if [ ! -f "Symbiosis 3.yyp" ]; then
+    cd $HOME/Documents
+    git clone https://github.com/Sp1d3rmanj0e/Regrown.git
+fi
 echo 'Succesfully cloned game! Starting GameMaker Studio...'
 'C:\Program Files\GameMaker Studio 2\GameMakerStudio.exe' $HOME/Documents/Regrown/'Symbiosis 3.yyp'
